019c3780efa8c8a4caae335daf9122ab1216bbb4
unknown <macjay314@gmail.com>
2020-05-04 10:31:56 -0700

    Create PingPong() skeleton

--- PingPong.cs
+++ PingPong.cs
@@ -12,10 +12,21 @@ class MealTime
     Console.WriteLine("Please enter a number:");
     string stringNumber = Console.ReadLine();
     int userNumber = int.Parse(stringNumber);
-    Console.WriteLine("");
+    PingPong(userNumber);
   }
 
   static void PingPong(int num)
   {
+    for (int i = 1; i < num; i++){
+      if (i % 3 == 0 && i % 5 == 0){
+        Console.WriteLine("ping-pong");
+      } else if (i % 3 == 0){
+        Console.WriteLine("ping");
+      } else if (i % 5 == 0){
+        Console.WriteLine("pong");
+      } else {
+        Console.WriteLine(i);
+      }
+    }
   }
 }
